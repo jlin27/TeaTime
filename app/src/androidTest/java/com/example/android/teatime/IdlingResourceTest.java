df5fc3da3935639706b0a1815c485960a1ce9722
jlin27 <jessica.lin@udacity.com>
2017-01-23 10:06:46 -0800

    Remove Matcher code from IdlingResourceTest

--- app/src/androidTest/java/com/example/android/teatime/IdlingResourceTest.java
+++ app/src/androidTest/java/com/example/android/teatime/IdlingResourceTest.java
@@ -5,13 +5,7 @@ import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -71,25 +65,7 @@ public class IdlingResourceTest {
 
 
     }
-
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
-    }
+    
 
     @After
     public void unregisterIdlingResource() {
