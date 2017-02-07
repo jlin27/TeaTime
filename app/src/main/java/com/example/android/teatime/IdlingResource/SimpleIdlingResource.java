9be6e03be3e0665fad49e89d6a88b5eafa2262e8
jlin27 <jessica.lin@udacity.com>
2017-02-06 17:32:08 -0800

    Adding Idling and it works when there's a button but doesn't when there's no button

--- app/src/main/java/com/example/android/teatime/IdlingResource/SimpleIdlingResource.java
+++ app/src/main/java/com/example/android/teatime/IdlingResource/SimpleIdlingResource.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2017 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*  	http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.teatime.IdlingResource;
 
@@ -23,6 +23,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * A very simple implementation of {@link IdlingResource}.
+ * <p>
+ * Consider using CountingIdlingResource from espresso-contrib package if you use this class from
+ * multiple threads or need to keep a count of pending operations.
  */
 
 public class SimpleIdlingResource implements IdlingResource {
@@ -48,10 +51,8 @@ public class SimpleIdlingResource implements IdlingResource {
     }
 
     /**
-     * Sets the new idle state.  Espresso waits until the app is "idle" before performing
-     * the next action and checking the next assertion. If isIdleNow is true, it pings
-     * the {@link ResourceCallback}. @param isIdleNow false if there are pending operations,
-     * true if idle.
+     * Sets the new idle state, if isIdleNow is true, it pings the {@link ResourceCallback}.
+     * @param isIdleNow false if there are pending operations, true if idle.
      */
     public void setIdleState(boolean isIdleNow) {
         mIsIdleNow.set(isIdleNow);
